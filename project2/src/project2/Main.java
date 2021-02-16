3092527d8bce32ee2a5ddadbb1e1416669e154ca
Lee-ChangHee <chbe5082@Naver.com>
2021-02-16 15:28:31 +0900

    update for CH

--- project2/src/project2/Main.java
+++ project2/src/project2/Main.java
@@ -1,14 +1,21 @@
 package project2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		div div = new div();
 		Scanner sc = new Scanner(System.in);
 		System.out.printf("number 1 :");
 		int num1 =  sc.nextInt();
 		System.out.printf("number 2 :");
 		int num2 =  sc.nextInt();
+
+		div.diveNum(num1, num2);
+		//
+
 	}
 
 }
---
+++ project2/src/project2/div.java
@@ -0,0 +1,10 @@
+package project2;
+
+public class div {
+
+    public double diveNum(int num1, int num2){
+        System.out.println("value : "+num1/num2);
+        return 0;
+        //
+    }
+}

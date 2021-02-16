65f339cc07e22b6af331f1e0310283b9e2a85155
Lee-ChangHee <chbe5082@Naver.com>
2021-02-16 15:19:10 +0900

    dwf

--- project2/src/project2/Main.java
+++ project2/src/project2/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("number 1 :");
+		int num1 =  sc.nextInt();
+		System.out.printf("number 2 :");
+		int num2 =  sc.nextInt();
 	}
 
 }

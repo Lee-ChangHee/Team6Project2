5bfb8d89d3041c71f5e9df1bb981a587b9dfdabe
suok <soook1004@naver.com>
2021-02-16 16:17:45 +0900

    뺄셈추가

--- project2/src/project2/Main.java
+++ project2/src/project2/Main.java
@@ -7,13 +7,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		div div = new div();
-<<<<<<< HEAD
+
 		plus p = new plus();
 		multi m = new multi();
-		
-=======
+		Minus minus = new Minus();
+
 		plus plus = new plus();
->>>>>>> 98c05b758854036fc788d2c30f5af55e3e4f450b
+
 		Scanner sc = new Scanner(System.in);
 		System.out.printf("number 1 :");
 		int num1 =  sc.nextInt();
@@ -21,12 +21,12 @@ public class Main {
 		int num2 =  sc.nextInt();
 
 		div.diveNum(num1, num2);
-<<<<<<< HEAD
+		minus.minusNum(num1, num2);
 		p.plusNum(num1,num2);
 		m.multiNum(num1, num2);
-=======
+
 		plus.plus(num1, num2);
->>>>>>> 98c05b758854036fc788d2c30f5af55e3e4f450b
+
 		//
 
 	}
---
+++ project2/src/project2/Minus.java
@@ -0,0 +1,9 @@
+package project2;
+
+public class Minus {
+	
+	public int minusNum(int num1,int num2) {
+		System.out.println("value = "+(num1-num2));
+		return 0;
+	}
+}

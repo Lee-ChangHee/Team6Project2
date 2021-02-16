656b3b881ed06e9cdcb0929c4f7b877d740cb574
Lee-ChangHee <chbe5082@Naver.com>
2021-02-16 15:16:21 +0900

    update 2

--- project2/src/project2/Main.java
+++ project2/src/project2/Main.java
@@ -1,9 +1,18 @@
 package project2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("number 1 :");
+		int num1 =  sc.nextInt();
+		System.out.printf("number 2 :");
+		int num2 =  sc.nextInt();
+		
+		//�Ѹ� ȣ��
+		
 		
 	}
 
